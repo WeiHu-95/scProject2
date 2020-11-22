c7989f0f0b9a9343eefb5152b22d352459eaf8b9
WeiHu-95 <73662349+WeiHu-95@users.noreply.github.com>
2020-11-22 02:08:53 +0000

    Update automation.sh

--- automation.sh
+++ automation.sh
@@ -7,4 +7,4 @@ activate
 rm -rf scProject2
 git clone https://github.com/WeiHu-95/scProject2.git
 cd scProject2
-python classify.py  --model-name test --captcha-dir HUWE-project2rpi --output stuff.txt --symbols symbols.txt
+python classify.py  --model-name model --captcha-dir HUWE-project2rpi --output stuff.txt --symbols symbols.txt
